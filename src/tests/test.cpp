e5f003d7b1a58eaa7d77721e11c6b2a723c16beb
Daniel Mitchell <daniel7@ualberta.ca>
2019-06-13 12:53:40 -0600

    Added support for local config overwriting

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -19,6 +19,11 @@ void from_json(const json& j, money& m) {
     j.at("currency").get_to(m.currency);
     j.at("value").get_to(m.value);
 }
+void combineJson(json& first, json& second) {
+    for (json::iterator it = second.begin(); it != second.end(); ++it) {
+      first[it.key()] = it.value();
+    }
+}
 
 int main() {
     // Switch s = Switch(1);
@@ -34,17 +39,32 @@ int main() {
     ifstream i("test.json");
     json j;
     i >> j;
-    float pi = j["pi"];
-    vector<int> list = j["list"];
-    cout << pi << endl;
-    for (auto it : list) {
-        cout << it << " ";
+    for (json::iterator it = j.begin(); it != j.end(); ++it) {
+      std::cout << it.key() << " : " << it.value() << "\n";
     }
-    cout << endl;
-    //money m = j["object"];
-    json o = j["object"];
-    auto m = o.get<money>();
-    cout << m.currency << endl << m.value << endl;
+    ifstream ii("local.json");
+    if (ii.good()) {
+        json jj;
+        ii >> jj;
+        combineJson(j, jj);
+    }
+    // for (json::iterator it = jj.begin(); it != jj.end(); ++it) {
+//       j[it.key()] = it.value();
+//     }
+    for (json::iterator it = j.begin(); it != j.end(); ++it) {
+      std::cout << it.key() << " : " << it.value() << "\n";
+    }
+    // float pi = j["pi"];
+//     vector<int> list = j["list"];
+//     cout << pi << endl;
+//     for (auto it : list) {
+//         cout << it << " ";
+//     }
+//     cout << endl;
+//     //money m = j["object"];
+//     json o = j["object"];
+//     auto m = o.get<money>();
+//     cout << m.currency << endl << m.value << endl;
 
     printf("Tests Passed!\n");
     return 0;
