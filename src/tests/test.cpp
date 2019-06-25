492be528a7bcc0dd8c5e7f494781837d8f3c1b5c
Aidan Bush <aidandbush@gmail.com>
2019-06-25 11:53:42 -0600

    Remove unused code in test.cpp

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -13,21 +13,7 @@
 
 using json = nlohmann::json;
 
-// Manager man;
-
-// struct money {
-//     string currency;
-//     float value;
-// };
-// void from_json(const json& j, money& m) {
-//     j.at("currency").get_to(m.currency);
-//     j.at("value").get_to(m.value);
-// }
-// void combineJson(json& first, json& second) {
-//     for (json::iterator it = second.begin(); it != second.end(); ++it) {
-//       first[it.key()] = it.value();
-//     }
-// }
+Manager man;
 
 int main() {
     int errors = 0;
@@ -60,42 +46,14 @@ int main() {
         printf("Endpoint tests passed\n");
     }
 
-    // TODO: Add config tests
-    // ifstream i("test.json");
-    // json j;
-    // i >> j;
-    // for (json::iterator it = j.begin(); it != j.end(); ++it) {
-    //   std::cout << it.key() << " : " << it.value() << "\n";
-    // }
-    // ifstream ii("local.json");
-    //
-    // if (ii.good()) {
-    //     json jj;
-    //     ii >> jj;
-    //     combineJson(j, jj);
-    // }
-    //
-    // for (json::iterator it = j.begin(); it != j.end(); ++it) {
-    //     std::cout << it.key() << " : " << it.value() << "\n";
-    // }
-    // float pi = j["pi"];
-    // vector<int> list = j["list"];
-    // cout << pi << endl;
-    // for (auto it : list) {
-    //     cout << it << " ";
-    // }
-    // cout << endl;
-    // //money m = j["object"];
-    // json o = j["object"];
-    // auto m = o.get<money>();
-    // cout << m.currency << endl << m.value << endl;
-
     if (!testSwitch()) {
         printf("Switch tests failed\n");
         errors++;
     } else {
         printf("Switch tests passed\n");
     }
+
+    // TODO: Add config tests
 
     if (errors == 0) {
         printf("All tests Passed!\n");
