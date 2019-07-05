d03fa549b9fc52522ecdae0272a4eb337c61ee8b
Daniel Mitchell <daniel7@ualberta.ca>
2019-07-05 14:39:17 -0600

    Moved validation into constructor for interfaces

--- src/interface.h
+++ src/interface.h
@@ -17,7 +17,7 @@ using json = nlohmann::json;
 
 class Interface: public NetworkObject {
     public:
-        Interface(int id, int handlerID, int linkBufSize, int handlerBufSize);
+        Interface(json interfaceConfig);
         void setLink(int linkID);
         void setLink(int linkId, vector<int> neighbours);
         int getLinkID();
@@ -41,6 +41,8 @@ class Interface: public NetworkObject {
         static int ifaceToIface();
 #endif /* _TEST */
     private:
+        static int validateInterfaceConfig(json interfaceConfig);
+
         bool validateHandler();
         bool validateLink();
         bool validateVariables();
