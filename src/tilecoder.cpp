eb7f50b5a97dc5531edb12579d2ccb04c616f800
Aidan Bush <aidandbush@gmail.com>
2020-07-27 14:07:53 -0600

    Add tilecoder

--- src/tilecoder.cpp
+++ src/tilecoder.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stdexcept>
 
 #include "tilecoder.h"
 
@@ -14,6 +15,7 @@
 
 using namespace std;
 
+/*
 namespace Tilecoder {
     double tileWidth = (STATE_MAX - STATE_MIN)/(NUM_TILES_PER_TILING - 1);
     double offset = tileWidth/NUM_TILINGS;
@@ -34,4 +36,143 @@ namespace Tilecoder {
     int getNumTiles() {
         return NUM_TILINGS*NUM_TILES_PER_TILING;
     }
+}
+*/
+
+Tilecoder::Tilecoder(int numDimensions, vector<pair<double, double>> dimensionRanges,
+        vector<vector<int>> tilecodingPatterns, vector<int> tilesPerDimTiling,
+        vector<int> numTilings) {
+    if (int(dimensionRanges.size()) != numDimensions) {
+        throw runtime_error("Number of dimensions and ranges don't match");
+    }
+
+    if (tilecodingPatterns.size() != tilesPerDimTiling.size() || tilecodingPatterns.size() != numTilings.size()) {
+        throw runtime_error("Number of patterns, tiles per, and tilings don't match");
+    }
+
+    // TODO check values in patterns
+
+    for (auto pattern: tilecodingPatterns) {
+        for (auto dim: pattern) {
+            if (dim < 0 || dim >= numDimensions) {
+                throw runtime_error("Invalid dimension in tilcoder pattern");
+            }
+        }
+    }
+
+    this->numDimensions = numDimensions;
+    this->ranges = dimensionRanges;
+    this->tilecodingPatterns = tilecodingPatterns;
+    this->tilesPerDimTiling = tilesPerDimTiling;
+    this->numTilings = numTilings;
+
+    // calculate tileWidths, and offsets
+    for (int i = 0; i < int(dimensionRanges.size()); i++) {
+        this->tileWidths.push_back((dimensionRanges[i].second - dimensionRanges[i].first) / (tilesPerDimTiling[i] - 1));
+        this->offsets.push_back(tileWidths[i] / numTilings[i]);
+    }
+
+    calculateNumTiles();
+
+    calculateNumTilings();
+}
+
+void Tilecoder::calculateNumTiles() {
+    int tiles = 0;
+    for (int i = 0; i < int(tilecodingPatterns.size()); i++) {
+        vector<int> pattern = tilecodingPatterns[i];
+
+        int curTiles = calculatePatternTiles(pattern, tilesPerDimTiling[i], numTilings[i]);
+
+        numPatternTiles.push_back(curTiles);
+        tiles += curTiles;
+    }
+
+    numTotaltiles = tiles;
+}
+
+int Tilecoder::calculatePatternTiles(vector<int> pattern, int tilesPerDimTiling, int numTilings) {
+    int size = 1;
+
+    for (int i = 0; i < int(pattern.size()); i++) {
+        size *= tilesPerDimTiling;
+    }
+
+    size *= numTilings;
+
+    return size;
+}
+
+void calculateNumTilings() {
+    for (auto tilings: numTilings) {
+        numTotalTilings += tilings;
+    }
+}
+
+int Tilecoder::getNumTiles() {
+    return totalSize;
+}
+
+int getNumTilings() {
+    return numTotalTilings;
+}
+
+vector<int> Tilecoder::tilecode(vector<double> state) {
+    // check number of dimensions
+    if (int(state.size()) != numDimensions) {
+        throw runtime_error("Number of dimensions and ranges don't match");
+    }
+
+    // normalize
+    for (int i = 0; i < numDimensions; i++) {
+        state[i] = (state[i] - ranges[i].first) / (ranges[i].second - ranges[i].first);
+    }
+
+    vector<int> tiles;
+
+    int patternOffset = 0;
+
+    // tilecode for every pattern
+    for (int i = 0; i < int(tilecodingPatterns.size()); i++) {
+        vector<int> pattern = tilecodingPatterns[i];
+        vector<double> currentState;
+
+        for (int j = 0; j < int(pattern.size()); j++) {
+            currentState.push_back(state[pattern[j]]);
+        }
+
+        vector<int> curTiles = tilecodeSingle(currentState, tilesPerDimTiling[i], numTilings[i], tileWidths[i],
+                offsets[i]);
+
+        // add tiles with offset
+        for (int i = 0; i < int(curTiles.size()); i++) {
+            tiles.push_back(curTiles[i] + patternOffset);
+        }
+
+        patternOffset += patternSizes[i];
+    }
+
+    return tiles;
+}
+
+vector<int> Tilecoder::tilecodeSingle(vector<double> state, int tilesPerDim, int numTilings, double tileWidth,
+        double offset) {
+    vector<int> tiles = vector<int>(numTilings);
+
+    for (int t = 0; t < int(tiles.size()); t++) {
+        int tile = 0;
+        int dimensionOffset = 1;
+
+        for (int d = 0; d < int(state.size()); d++) {
+            tile += int((state[d] + offset * t) / tileWidth) * dimensionOffset;
+            dimensionOffset *= tilesPerDim;
+        }
+
+        // tiling offset
+        tile += t * dimensionOffset;
+
+        tiles[t] = tile;
+    }
+
+    return tiles;
 }
--- src/tilecoder.h
+++ src/tilecoder.h
@@ -3,9 +3,36 @@
 
 using namespace std;
 
-namespace Tilecoder {
-    vector<int> tilecode(double state);
-    int getNumTilings();
-    int getNumTiles();
-}
+class Tilecoder {
+    public:
+        Tilecoder(int numDimensions, vector<pair<double, double>> dimensionRanges,/* min, max */
+            vector<vector<int>> tilecodingPatterns, vector<int> tilesPerDimTiling,
+            vector<int> numTilings);
+
+        vector<int> tilecode(vector<double> state);
+
+        int getNumTiles();
+        int getNumTilings();
+
+    private:
+        void calculateNumTiles();
+        int calculatePatternTiles(vector<int> pattern, int tilesPerDimTiling, int numTilings);
+
+        vector<int> tilecodeSingle(vector<double> state, int tilesPerDim, int numTilings, double tileWidth, double offset);
+
+        int numDimensions;
+        vector<pair<double, double>> ranges;
+        vector<vector<int>> tilecodingPatterns;
+        vector<int> tilesPerDimTiling;
+        vector<int> numTilings;
+
+        int numTotalTiles;
+        int numTotalTilings;
+        vector<int> numPatternTile;
+
+        vector<double> tileWidths;
+        vector<double> offsets;
+};
+
+
 #endif /* TILECODER_H */
