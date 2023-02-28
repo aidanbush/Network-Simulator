69a3bcb4f3d62ee4b0eabf54d5be12d5af662c4e
aidan <aidandbush@gmail.com>
2023-02-28 14:44:27 -0700

    Refactor other_plots to use the new data format

--- other_plots.py
+++ other_plots.py
@@ -27,17 +27,22 @@ def multiple_run_drop_rate():
 
 def multiple_run_plot(mean_field, stdev_field, fig_type, ylim):
     utilization = "0.4"
-    fig_name = f"8x8 Bursty {utilization} Utilization {fig_type}"
+    #fig_name = f"8x8 Bursty {utilization} Utilization {fig_type}"
+    fig_name = f"5x5 Bursty {utilization} Utilization {fig_type}"
     #fig_name = f"5x5 Bursty {utilization} fs 4 Utilization {fig_type}"
+    #fig_name = f"5x5 Bursty {utilization} fc 20 Utilization {fig_type}"
     output_name = fig_name.replace(' ', '_')
 
     directory = "results/"
-    run_prefix = f"8x8_bursty_{utilization}_"
+    #run_prefix = f"8x8_bursty_{utilization}_"
+    run_prefix = f"5x5_bursty_{utilization}_"
     #run_infixes = ["mbd_[1-2_hop_shortest]", "mbd_[dest_id]", "rand_deflect", "mbd_[1_hop_shortest]", "mbd_[2_hop_shortest]", "mbd_[2_hop_shortest,1_hop_shortest]", "mbd_[flow_id]"]
-    #run_infixes = ["mbd_[2_hop_shortest,1_hop_shortest]", "mbd_[1_hop_shortest]", "mbd_[dest_id]", "rand_deflect", "rand_forward"]
-    run_infixes = ["mbd_[2_hop_shortest,1_hop_shortest]", "mbd_[1_hop_shortest]", "mbd_[dest_id]", "mbd_[1_hop_shortest,3x3_section]", "rand_deflect", "rand_forward"]
+    run_infixes = ["mbd_[2_hop_shortest,1_hop_shortest]", "mbd_[1_hop_shortest]", "mbd_[dest_id]", "rand_deflect", "rand_forward"]
+    #run_infixes = ["mbd_[2_hop_shortest,1_hop_shortest]", "mbd_[1_hop_shortest]", "mbd_[dest_id]", "mbd_[1_hop_shortest,3x3_section]", "rand_deflect", "rand_forward"]
+    #run_infixes = ["mbd_[1_hop_shortest]", "rand_deflect", "rand_forward"]
     run_suffix = ""
     #run_suffix = "_fs_4"
+    #run_suffix = "_fc_20"
     file_suffix = "/results.csv"
 
     run_infixes = sorted(run_infixes)
@@ -78,9 +83,13 @@ def multiple_run_plot(mean_field, stdev_field, fig_type, ylim):
                     if val != 0.0: # ignore 0s as they are from flows that are not currently on
                         row_stdevs.append(val)
 
-                means.append(statistics.mean(row_means))
+                means.append(row_means)
+                stdevs.append(row_stdevs)
+
+                # uncomment 1 and 3 for only flow reporting results
+                #means.append(statistics.mean(row_means))
                 #stdevs.append((sum([stdev**2 for stdev in row_stdevs]))**.5)
-                stdevs.append(statistics.stdev(row_means))
+                #stdevs.append(statistics.stdev(row_means))
 
         data[run_name] = (np.array(means), np.array(stdevs))
 
