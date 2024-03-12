cf219eca11c139cdee30f58ccdd5e429600ee2c9
aidan <aidandbush@gmail.com>
2024-03-12 17:52:05 +0000

    Add deflection limit to random deflection baseline

--- genGridConfig.py
+++ genGridConfig.py
@@ -483,6 +483,9 @@ def gen_path(dest_dir, size, experiment_config):
             filepath = os.path.join(filepath, "of")
     elif experiment_config["type"] == "rand_deflect":
         filepath = os.path.join(filepath, f"rand_deflect")
+        sd = experiment_config["static_deflections"]
+        if sd != -1:
+            filepath = os.path.join(filepath, f"sd_{sd}")
     elif experiment_config["type"] == "rand_forward":
         filepath = os.path.join(filepath, f"rand_forward")
     else:
@@ -554,7 +557,7 @@ def setup_configs(size, experiment_config):
         pass
     elif experiment_config["type"] == "rand_deflect":
         switchConfig["deflect_thresh"] = 1.0
-        flowConfig["static_deflections"] = 2
+        flowConfig["static_deflections"] = experiment_config["static_deflections"]
 
 def gen_config_list(net_utils, prop_delay, traffic_types, agent_types, mbd_agent_algs, mbd_states,
         mbd_regularizer, mbd_delta, mbd_discount_factor, mbd_static_deflect, ndd_alg, ndd_alpha, ndd_epsilon,
@@ -598,7 +601,7 @@ def gen_config_list(net_utils, prop_delay, traffic_types, agent_types, mbd_agent
         agent_dicts += rand_forward_dict
 
     if "rand_deflect" in agent_types:
-        rand_deflect_dict = [{"type": "rand_deflect", "flow_type": "rand_deflect"}]
+        rand_deflect_dict = [{"type": "rand_deflect", "flow_type": "rand_deflect", "static_deflections": 2}]
         agent_dicts += rand_deflect_dict
 
     experiment_dicts = [{**d, **{"net_util": u, "prop_delay": p, "traffic_type": t}}
@@ -619,7 +622,7 @@ def main():
     traffic_types = [TRAFFIC_MICE_ELEPHANT, TRAFFIC_CHANGING, TRAFFIC_STATIC][0:1]
     net_utils = [0.05, 0.1, 0.15, 0.2][0:1] # [0.1,0.2,0.3,0.4]
     prop_delays = [0.01,0.1,0.5][0:1]
-    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][1:2]
+    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][3:4]
     mbd_agent_algs = ["original", "slide", "D-LinUCB", None][1:2]
     mbd_regularizers = [0.5,0.9,1.0,1.1,2.0][2:3]
     mbd_deltas = [0.1,0.5,0.9,1.0][3:4]
@@ -643,7 +646,7 @@ def main():
     ndd_alphas = [0.05,0.01,0.005][0:1]
     ndd_epsilons = [0.05,0.01,0.005][0:1]
     ndd_gammas = [0.99][0:1]
-    ndd_only_forward = [False, True][0]
+    ndd_only_forward = [False, True][1]
 
     experiment_configs = gen_config_list(net_utils, prop_delays, traffic_types, agent_types,
             mbd_agent_algs, mbd_states, mbd_regularizers, mbd_deltas, mbd_discount_factors,
