d6ae051b20e1d95a4ed9fc79efc6d7e5e65d050f
aidan <aidandbush@gmail.com>
2024-07-01 01:46:01 +0000

    Fix NDD data not reseting properly

--- src/switch.cpp
+++ src/switch.cpp
@@ -1561,6 +1561,8 @@ void NDDSwitch::resetData() {
     this->learningActionsTaken = 0;
     this->rewardSum = 0.0;
     this->actionsRewarded = 0;
+
+    Switch::resetData();
 }
 
 void NDDSwitch::recordData() {
