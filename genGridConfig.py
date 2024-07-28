ad76754f885d8fe16129717cdc8b244522fc11d9
aidan <aidandbush@gmail.com>
2024-07-28 20:48:34 +0000

    Add ndd sweep and support for less frequent mbd updates

--- genGridConfig.py
+++ genGridConfig.py
@@ -557,7 +557,7 @@ def create_config(filepath, size, net_util, simulation_length, flow_gen_type, ru
 
         if network_type == NETWORK_2D:
             gen_2d_network(size, config)
-        if network_type == NETWORK_2D_CUT:
+        elif network_type == NETWORK_2D_CUT:
             gen_2d_network(size, config, remove_mid=True)
         elif network_type == NETWORK_3D:
             gen_3d_network(size, config)
@@ -625,6 +625,8 @@ def gen_path(dest_dir, size, network_type, experiment_config):
         mbd_entropy_interval = experiment_config["entropy_intervals"]
         if experiment_config["action_limit"] != "none":
             filepath = os.path.join(filepath, experiment_config["action_limit"])
+        if experiment_config["mean_update_interval"] != 1:
+            filepath = os.path.join(filepath, f"m_ui_{experiment_config['mean_update_interval']}")
 
         mbd_entropy_string = ",".join(map(str, mbd_entropy_interval))
         filepath = os.path.join(filepath, f"s_{states}",
@@ -698,6 +700,7 @@ def setup_configs(size, experiment_config):
         switchConfig["drop_action"] = False
         switchConfig["action_limit"] = experiment_config["action_limit"]
         switchConfig["entropy_intervals"] = experiment_config["entropy_intervals"]
+        switchConfig["mean_update_interval"] = experiment_config["mean_update_interval"]
 
         switchConfig["agent_alg"] = experiment_config["mbd_alg"]
         switchConfig["states"] = experiment_config["states"]
@@ -730,8 +733,8 @@ def setup_configs(size, experiment_config):
         flowConfig["static_deflections"] = experiment_config["static_deflections"]
 
 def gen_config_list(net_utils, prop_delay, traffic_types, agent_types, mbd_agent_algs, mbd_states,
-        mbd_hyper_params, mbd_static_deflect, mbd_action_limits, mbd_entropy_interval, ndd_alg,
-        ndd_hyper_params, ndd_only_forward, ndd_multiple_updates, rand_deflect_static_deflects):
+        mbd_hyper_params, mbd_static_deflect, mbd_action_limits, mbd_mean_update_interval, mbd_entropy_interval,
+        ndd_alg, ndd_hyper_params, ndd_only_forward, ndd_multiple_updates, rand_deflect_static_deflects):
 
     agent_dicts = []
 
@@ -754,9 +757,10 @@ def gen_config_list(net_utils, prop_delay, traffic_types, agent_types, mbd_agent
     if "mbd" in agent_types:
         mbd_algs = []
         mbd_dict = [{"type": "mbd", "flow_type": "mbd", "drop_action": False,
-                "static_deflections": sd, "states": s, "action_limit": al, "entropy_intervals": ei}
+                "static_deflections": sd, "states": s, "action_limit": al,
+                "mean_update_interval": ui, "entropy_intervals": ei}
                 for sd in mbd_static_deflect for s in mbd_states for al in mbd_action_limits
-                for ei in mbd_entropy_interval]
+                for ui in mbd_mean_update_interval for ei in mbd_entropy_interval]
 
         if "D-LinUCB" in mbd_agent_algs:
             mbd_algs += [{**d, **{"mbd_alg": "D-LinUCB", "regularizer": r, "delta": de, "discount_factor": df}}
@@ -796,7 +800,7 @@ def main():
     runs = 30
     simulation_length = 1000 # 2000
     num_flow_changes = 200 # 100
-    network_type = [NETWORK_2D, NETWORK_2D_CUT, NETWORK_3D][1]
+    network_type = [NETWORK_2D, NETWORK_2D_CUT, NETWORK_3D][0]
 
     flowConfigDefault["ttl"] = size * 3
 
@@ -807,7 +811,7 @@ def main():
     net_utils = [0.1, 0.2][0:2] # [0.1,0.2,0.3,0.4]
     prop_delays = [0.001,0.01,0.1,1.0][1:2]
     #prop_delays = [0.001,0.1,1.0]
-    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][2:3]
+    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][0:1]
     mbd_agent_algs = ["original", "slide", "D-LinUCB", None][0:1]
     mbd_hyper_params = [
             # regularizer, delta, discount factor
@@ -825,8 +829,8 @@ def main():
     #mbd_regularizers = [0.5,0.9,1.0,1.1,2.0][2:3]
     #mbd_deltas = [0.1,0.5,0.9,1.0][3:4]
     #mbd_discount_factors = [0.99, 0.999, 0.9999][1:2]
-    mbd_static_deflect = [-1,2,4,6,8][4:5]
-    mbd_action_limits = ["none", "forward_first", "only_deflect"][0:2]
+    mbd_static_deflect = [-1,2,4,6,8][0:1]
+    mbd_action_limits = ["none", "forward_first", "only_deflect", "only_forward"][0:1]
     mbd_states = [
             ["1_hop_shortest"],
             ["1-2_hop_shortest"],
@@ -840,13 +844,14 @@ def main():
             ["2_hop_shortest", "1_hop_shortest", "section", "drop_probability"],
             ["dest_id"],
             ["dest_id", "deflect_probability"],
-            ["dest_id", "drop_probability"], ["flow_id"]][2:3]#[1:12]#[1:14]#[3:8]
+            ["dest_id", "drop_probability"], ["flow_id"]][2:3]#[3:4]
+    mbd_mean_update_interval = [1,2,4,8,16,32][4:6]
     mbd_entropy_interval = [[1,2,4,6,8],[5]][1:2]
     ndd_algs = ["rand", "Q-learning"][1:2]
     ndd_hyper_params =[
             #alpha, epsilon, gamma
-            [0.5,0.05,0.99],
-            ]
+            [0.100, 0.050, 0.990],
+            [0.006, 0.023, 0.685], [0.081, 0.046, 0.999], [0.002, 0.059, 0.996], [0.013, 0.066, 0.958], [0.050, 0.012, 0.993], [0.009, 0.029, 0.131], [0.021, 0.058, 0.997], [0.002, 0.047, 0.997], [0.096, 0.010, 0.748], [0.088, 0.015, 0.923], [0.038, 0.044, 0.971], [0.091, 0.031, 0.997], [0.004, 0.032, 0.988], [0.009, 0.014, 0.989], [0.031, 0.008, 0.999], [0.002, 0.010, 0.914], [0.082, 0.018, 0.968], [0.002, 0.022, 0.952], [0.010, 0.089, 0.992], [0.002, 0.084, 0.994], [0.016, 0.064, 0.998], [0.015, 0.052, 0.990], [0.009, 0.010, 0.560], [0.004, 0.005, 0.998], ][0:11]
     # ranges alpha [0.1, 0.0001] -> 10-10000 steps - log
     # ranges epsilon [0.05, 0.001] -> 20-200 steps - log
     # ranges gamma [0.99,0.9] -> ?-? log
@@ -855,13 +860,13 @@ def main():
     #   for low, high in [[0.0001,0.1],[0.001,0.05],[0.9,0.99]]] # loop over ranges
     ndd_only_forward = [False, True][0:1]
     ndd_multiple_updates = [False, True][1:2]
-    rand_deflect_static_deflects = [-1,2][0:2]
+    rand_deflect_static_deflects = [-1,2,4,6,8][0:5]
 
     experiment_configs = gen_config_list(net_utils, prop_delays, traffic_types,
             agent_types, mbd_agent_algs, mbd_states, mbd_hyper_params,
-            mbd_static_deflect, mbd_action_limits, mbd_entropy_interval,
-            ndd_algs, ndd_hyper_params, ndd_only_forward, ndd_multiple_updates,
-            rand_deflect_static_deflects)
+            mbd_static_deflect, mbd_action_limits, mbd_mean_update_interval,
+            mbd_entropy_interval, ndd_algs, ndd_hyper_params, ndd_only_forward,
+            ndd_multiple_updates, rand_deflect_static_deflects)
 
     for experiment_config in experiment_configs:
         setup_configs(size, experiment_config)
--- many_runs.py
+++ many_runs.py
@@ -32,6 +32,8 @@ def gen_experiment_name(size, config, network_type):
                 + "_ei_" + ",".join(map(str, config["entropy_intervals"]))
         if config["action_limit"] != "none":
             agent += "_" + config["action_limit"]
+        if config["mean_update_interval"] != 1:
+            agent += "_m_ui_" + str(config["mean_update_interval"])
     elif agent == "NDD":
         ndd_type = config["ndd_alg"]
         agent += f"_{ndd_type}"
@@ -59,14 +61,14 @@ def main():
     num_runs = 30
     parallel = 40
     config_dir = "configs"
-    network_type = [helper.NETWORK_2D, helper.NETWORK_2D_CUT, helper.NETWORK_3D][1]
+    network_type = [helper.NETWORK_2D, helper.NETWORK_2D_CUT, helper.NETWORK_3D][0]
 
     traffic_types = [helper.TRAFFIC_MICE_ELEPHANT, helper.TRAFFIC_CHANGING, helper.TRAFFIC_STATIC][0:1]
     net_utils = [0.05, 0.1, 0.15, 0.2, 0.25][0:2]
     net_utils = [0.1, 0.2][0:2] # [0.1,0.2,0.3,0.4]
     prop_delays = [0.01,0.1,0.5][0:1]
     #prop_delays = [0.001,0.1,1.0]
-    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][2:3]
+    agent_types = ["mbd", "NDD", "rand_forward", "rand_deflect"][0:1]
     mbd_agent_algs = ["original", "slide", "D-LinUCB", None][0:1]
     mbd_hyper_params = [
             # regularizer, delta, discount factor
@@ -83,8 +85,8 @@ def main():
     #mbd_regularizers = [0.5,0.9,1.0,1.1,2.0][2:3]
     #mbd_deltas = [0.1,0.5,0.9,1.0][3:4]
     #mbd_discount_factors = [0.99, 0.999, 0.9999][3:4]
-    mbd_static_deflect = [-1,2,4,6,8][4:5]
-    mbd_action_limits = ["none", "forward_first", "only_deflect"][0:2]
+    mbd_static_deflect = [-1,2,4,6,8][0:1]
+    mbd_action_limits = ["none", "forward_first", "only_deflect", "only_forward"][0:1]
     mbd_states = [
             ["1_hop_shortest"],
             ["1-2_hop_shortest"],
@@ -98,13 +100,14 @@ def main():
             ["2_hop_shortest", "1_hop_shortest", "section", "drop_probability"],
             ["dest_id"],
             ["dest_id", "deflect_probability"],
-            ["dest_id", "drop_probability"], ["flow_id"]][2:3]#[0:3][3:5]
+            ["dest_id", "drop_probability"], ["flow_id"]][2:3]#[3:4]
+    mbd_mean_update_interval = [1,2,4,8,16,32][4:6]
     mbd_entropy_interval = [[1,2,4,6,8],[5]][1:2]
     ndd_algs = ["rand", "Q-learning"][1:2]
     ndd_hyper_params =[
             #alpha, epsilon, gamma
-            [0.5,0.05,0.99],
-            ]
+            [0.100, 0.050, 0.990],
+            [0.006, 0.023, 0.685], [0.081, 0.046, 0.999], [0.002, 0.059, 0.996], [0.013, 0.066, 0.958], [0.050, 0.012, 0.993], [0.009, 0.029, 0.131], [0.021, 0.058, 0.997], [0.002, 0.047, 0.997], [0.096, 0.010, 0.748], [0.088, 0.015, 0.923], [0.038, 0.044, 0.971], [0.091, 0.031, 0.997], [0.004, 0.032, 0.988], [0.009, 0.014, 0.989], [0.031, 0.008, 0.999], [0.002, 0.010, 0.914], [0.082, 0.018, 0.968], [0.002, 0.022, 0.952], [0.010, 0.089, 0.992], [0.002, 0.084, 0.994], [0.016, 0.064, 0.998], [0.015, 0.052, 0.990], [0.009, 0.010, 0.560], [0.004, 0.005, 0.998], ][0:11]
     # ranges alpha [0.1, 0.0001] -> 10-10000 steps - log
     # ranges epsilon [0.05, 0.001] -> 20-200 steps - log
     # ranges gamma [0.99,0.9] -> ?-? log
@@ -113,13 +116,14 @@ def main():
     #   for low, high in [[0.0001,0.1],[0.001,0.05],[0.9,0.99]]] # loop over ranges
     ndd_only_forward = [False, True][0:1]
     ndd_multiple_updates = [False, True][1:2]
-    rand_deflect_static_deflects = [-1,2][0:2]
+    rand_deflect_static_deflects = [-1,2,4,6,8][0:5]
 
     experiment_configs = helper.gen_config_list(net_utils, prop_delays,
             traffic_types, agent_types, mbd_agent_algs, mbd_states,
             mbd_hyper_params, mbd_static_deflect, mbd_action_limits,
-            mbd_entropy_interval, ndd_algs, ndd_hyper_params, ndd_only_forward,
-            ndd_multiple_updates, rand_deflect_static_deflects)
+            mbd_mean_update_interval, mbd_entropy_interval, ndd_algs,
+            ndd_hyper_params, ndd_only_forward, ndd_multiple_updates,
+            rand_deflect_static_deflects)
 
     for config in experiment_configs:
         run_name = gen_experiment_name(size, config, network_type)
